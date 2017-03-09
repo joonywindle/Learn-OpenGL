f1e268978e2b9100d7c3db52f33eb8fe8785e7f8
Jonathan Windle <joony.windle@googlemail.com>
2017-03-09 15:18:07 +0000

    made a 3d cube, spin with depth tests

--- main.cpp
+++ main.cpp
@@ -46,6 +46,7 @@ int main()
         std::cout << "Failed to initialize GLEW" << std::endl;
         return -1;
     }
+    glEnable(GL_DEPTH_TEST);
 
     // Define the viewport dimensions
     int fwidth, fheight;
@@ -54,10 +55,45 @@ int main()
     glViewport(0, 0, fwidth, fheight);
     Shader ourShader("texture.vs","texture.frag");
     GLfloat vertices[] = {
-     0.5f,0.5f,0.0f, 1.0f,0.0f,0.0f,1.0f,1.0f,
-      0.5f,-0.5f,0.0f, 0.0f,1.0f,0.0f,1.0f,0.0f,
-      -0.5f,-0.5f,0.0f, 0.0f,0.0f,1.0f,0.0f,0.0f,
-      -0.5f,0.5f,0.0f, 1.0f, 1.0f,0.0f,0.0f,1.0f
+      -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+           0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+                0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+                     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+                         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+                             -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+          -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+               0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+                    0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+                         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+                             -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+                                 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+
+                                     -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+                                         -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+                                             -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+                                                 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+                                                     -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+                                                         -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+          0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+               0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+                    0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+                         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+                              0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+              0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+                   0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+                       -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+                           -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+                               -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+                                    0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+                                         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+                                              0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+                                                  -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+                                                      -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
     GLint indeces[] {
       0,1,3,
@@ -66,17 +102,17 @@ int main()
     GLuint VAO,VBO,EBO;
     glGenVertexArrays(1,&VAO);
     glGenBuffers(1,&VBO);
-    glGenBuffers(1,&EBO);
+//    glGenBuffers(1,&EBO);
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices),vertices,GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indeces), indeces, GL_STATIC_DRAW);
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,8*sizeof(GLfloat),(GLvoid*)0);
+//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+//    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indeces), indeces, GL_STATIC_DRAW);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,5*sizeof(GLfloat),(GLvoid*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,8*sizeof(GLfloat),(GLvoid*)(3*sizeof(GLfloat)));
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(2,2,GL_FLOAT,GL_FALSE,8*sizeof(GLfloat),(GLvoid*)(6*sizeof(GLfloat)));
+  //  glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,8*sizeof(GLfloat),(GLvoid*)(3*sizeof(GLfloat)));
+  //  glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2,2,GL_FLOAT,GL_FALSE,5*sizeof(GLfloat),(GLvoid*)(3*sizeof(GLfloat)));
     glEnableVertexAttribArray(2);
     glBindVertexArray(0);
 
@@ -99,7 +135,6 @@ int main()
 
     glm::vec4 vec(1.0f,0.0f,0.0f,1.0f);
     // Game loop
-    int count = 0;
     GLfloat pos = 0.5f;
     while (!glfwWindowShouldClose(window))
     {
@@ -108,19 +143,29 @@ int main()
         // Swap the screen buffers
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glBindTexture(GL_TEXTURE_2D, texture);
         ourShader.Use();
-   glm::mat4 trans;
-   glm::mat4 view;
-   trans = glm::translate(trans,glm::vec3(0.0f,0.0f,0.0f));
-   count+=90;
-    trans = glm::rotate(trans,(GLfloat)glfwGetTime() * glm::radians(180.0f),glm::vec3(0.0,0.0,1.0));
-//    trans = glm::scale(trans,glm::vec3(0.5,0.5,0.5));
-    GLuint transformLogic = glGetUniformLocation(ourShader.Program, "transform");
-       glUniformMatrix4fv(transformLogic,1,GL_FALSE,glm::value_ptr(trans));
-      glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glm::mat4 trans;
+        glm::mat4 view;
+        glm::mat4 projection;
+        glm::mat4 model;
+        model = glm::rotate(model, (GLfloat)glfwGetTime() * glm::radians(-55.0f), glm::vec3(0.5f,1.0f,0.0f));
+        view = glm::translate(view,glm::vec3(0.0f,0.0f,-3.0f));
+        projection = glm::perspective(glm::radians(45.0f),(float)WIDTH/(float)HEIGHT, 0.1f,100.0f);
+        GLint modelLogic = glGetUniformLocation(ourShader.Program, "model");
+        GLint viewLogic = glGetUniformLocation(ourShader.Program, "view");
+        GLint projLogic = glGetUniformLocation(ourShader.Program, "projection");
+
+
+        //trans = glm::rotate(trans,(GLfloat)glfwGetTime() * glm::radians(180.0f),glm::vec3(0.0,0.0,1.0));
+//      trans = glm::scale(trans,glm::vec3(0.5,0.5,0.5));
+      //  GLuint transformLogic = glGetUniformLocation(ourShader.Program, "transform");
+        glUniformMatrix4fv(modelLogic,1,GL_FALSE,glm::value_ptr(model));
+        glUniformMatrix4fv(viewLogic,1,GL_FALSE,glm::value_ptr(view));
+        glUniformMatrix4fv(projLogic,1,GL_FALSE,glm::value_ptr(projection));
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES,0, 36);
         glBindVertexArray(0);
         glfwSwapBuffers(window);
 
@@ -128,7 +173,7 @@ int main()
     }
     glDeleteVertexArrays(1,&VAO);
     glDeleteBuffers(1,&VBO);
-    glDeleteBuffers(2,&EBO);
+//    glDeleteBuffers(2,&EBO);
     // Terminate GLFW, clearing any resources allocated by GLFW.
     glfwTerminate();
     return 0;
