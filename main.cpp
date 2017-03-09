bee98a70ba9c9ac18d776aaa138ef64c671ae8b8
Jonathan Windle <joony.windle@googlemail.com>
2017-03-09 17:23:41 +0000

    Added camera view and rotation on an axis

--- main.cpp
+++ main.cpp
@@ -99,6 +99,18 @@ int main()
       0,1,3,
       1,2,3
     };
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+          glm::vec3( 2.0f,  5.0f, -15.0f),
+            glm::vec3(-1.5f, -2.2f, -2.5f),
+              glm::vec3(-3.8f, -2.0f, -12.3f),
+                glm::vec3( 2.4f, -0.4f, -3.5f),
+                  glm::vec3(-1.7f,  3.0f, -7.5f),
+                    glm::vec3( 1.3f, -2.0f, -2.5f),
+                      glm::vec3( 1.5f,  2.0f, -2.5f),
+                        glm::vec3( 1.5f,  0.2f, -1.5f),
+                          glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
     GLuint VAO,VBO,EBO;
     glGenVertexArrays(1,&VAO);
     glGenBuffers(1,&VBO);
@@ -146,26 +158,45 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glBindTexture(GL_TEXTURE_2D, texture);
         ourShader.Use();
+        glm::vec3 cameraPos = glm::vec3(0.0f,0.0f,3.0f);
+        glm::vec3 cameraTarget = glm::vec3(0.0f,0.0f,0.0f);
+        glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+        glm::vec3 up = glm::vec3(0.0f,1.0f,0.0f);
+        glm::vec3 cameraRight = glm::normalize(glm::cross(up,cameraDirection));
+        glm::vec3 cameraUp = glm::cross(cameraDirection,cameraRight);
         glm::mat4 trans;
-        glm::mat4 view;
         glm::mat4 projection;
-        glm::mat4 model;
-        model = glm::rotate(model, (GLfloat)glfwGetTime() * glm::radians(-55.0f), glm::vec3(0.5f,1.0f,0.0f));
-        view = glm::translate(view,glm::vec3(0.0f,0.0f,-3.0f));
-        projection = glm::perspective(glm::radians(45.0f),(float)WIDTH/(float)HEIGHT, 0.1f,100.0f);
+        glm::mat4 view;
         GLint modelLogic = glGetUniformLocation(ourShader.Program, "model");
         GLint viewLogic = glGetUniformLocation(ourShader.Program, "view");
         GLint projLogic = glGetUniformLocation(ourShader.Program, "projection");
-
-
-        //trans = glm::rotate(trans,(GLfloat)glfwGetTime() * glm::radians(180.0f),glm::vec3(0.0,0.0,1.0));
-//      trans = glm::scale(trans,glm::vec3(0.5,0.5,0.5));
-      //  GLuint transformLogic = glGetUniformLocation(ourShader.Program, "transform");
-        glUniformMatrix4fv(modelLogic,1,GL_FALSE,glm::value_ptr(model));
+        //view = glm::translate(view,glm::vec3(0.0f,0.0f,-3.0f));
+        GLfloat radius = 10.0f;
+        GLfloat camX = sin(glfwGetTime()) * radius;
+        GLfloat camY = cos(glfwGetTime()) * radius;
+        view = glm::lookAt(glm::vec3(camX,0.0f,camY), glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.0f,1.0f,0.0f));
+        projection = glm::perspective(glm::radians(45.0f),(float)WIDTH/(float)HEIGHT, 0.1f,100.0f);
         glUniformMatrix4fv(viewLogic,1,GL_FALSE,glm::value_ptr(view));
         glUniformMatrix4fv(projLogic,1,GL_FALSE,glm::value_ptr(projection));
+
+
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES,0, 36);
+        for(GLint i = 0; i < 10; i++){
+          glm::mat4 model;
+          model = glm::translate(model,cubePositions[i]);
+          GLfloat angle = 20.0f * i;
+          if(i%3 == 0)
+            angle = glfwGetTime() * 25.0f;
+
+          model = glm::rotate(model,glm::radians(angle),glm::vec3(1.0f,0.3f,0.5f));
+
+          glUniformMatrix4fv(modelLogic,1,GL_FALSE,glm::value_ptr(model));
+          glDrawArrays(GL_TRIANGLES,0, 36);
+        }
+                //trans = glm::rotate(trans,(GLfloat)glfwGetTime() * glm::radians(180.0f),glm::vec3(0.0,0.0,1.0));
+//      trans = glm::scale(trans,glm::vec3(0.5,0.5,0.5));
+      //  GLuint transformLogic = glGetUniformLocation(ourShader.Program, "transform");
+       //glBindVertexArray(VAO);
         glBindVertexArray(0);
         glfwSwapBuffers(window);
 
